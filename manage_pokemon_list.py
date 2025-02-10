b3112dfc7f847b0b4b7c21a566bb426c12142ecb
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-02-10 17:15:25 +0100

    adding some changes to manage_pokemon_list

--- manage_pokemon_list.py
+++ manage_pokemon_list.py
@@ -17,7 +17,8 @@ class Pokemon_bank:
                 pokemon_list = json.load(fichier)
         except FileNotFoundError:
                 pokemon_list = []
-
+        return pokemon_list
+# Build the list updated
     def record_new_pokemon(self,pokemon):
           self.pokemon_list.append(pokemon) 
 
@@ -29,13 +30,35 @@ lugia =  { "name" : "Lugia", "lifepoint": 10,"level": 1,
                              "experience": 0, "attack": 1, "defense" : 1, "type": "Air"}
  
 
-salameche = {"salameche" : { "name" : "Salameche", "lifepoint": 10,"level": 1, 
-                             "experience": 0, "attack": 1, "defense" : 1, "type": "Feu"}}
+salameche = {"name" : "Salameche", "lifepoint": 10,"level": 1, 
+                             "experience": 0, "attack": 1, "defense" : 1, "type": "Feu"}
 
+carapuce = {"name" : "Carapuce", "lifepoint": 10,"level": 1, 
+                             "experience": 0, "attack": 1, "defense" : 1, "type": "Feu"}
+roudoudou = {"name" : "Roudoudou", "lifepoint": 10,"level": 1, 
+                             "experience": 0, "attack": 1, "defense" : 1, "type": "Feu"}
+
+
+
+
+
+""" Building a Bank using the Pokemon_Bank class"""
+
+# a new Pokemon_Bank instance
 original_bank = Pokemon_bank("Pokemon Bank")
+
+# Test le contenu de la liste
+pokemon_list= original_bank.get_pokemon_list()
+print(pokemon_list)
+
+# records in a list with class method
 original_bank.record_new_pokemon(pikachu)
 original_bank.record_new_pokemon(lugia)
+original_bank.record_new_pokemon(salameche)
+original_bank.record_new_pokemon(carapuce)
+original_bank.record_new_pokemon(roudoudou)
 
+# Record the list in The Json File
 original_bank.record_pokemon()
 
 
