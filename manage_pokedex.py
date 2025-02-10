74935122e890e0f439247aacee2559791eb1fd32
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-02-10 16:42:26 +0100

    mute manage_pokedex file to Pokedex class

--- manage_pokedex.py
+++ manage_pokedex.py
@@ -3,18 +3,27 @@ import json
 
 """ Manage POKEDEX Json file """
 
-""" Constitution of the starting deck """
-
-def choose_pokemon_random():
-    with open('pokemon.json', 'r') as file:
-        pokemon_list = json.load(file)
+class Pokedex:
+    def __init__(self, name):
+        self.name = name
+        self.pokemon_list = []
+    
+    def choose_pokemon_random(self):
+        with open('pokemon.json', 'r') as file:
+            self.pokemon_list = json.load(file)
        
-    pokemon_sample = random.sample(pokemon_list, 1) # Randomly chooses 1 element from pokemon_list
-    return pokemon_sample
+        pokemon_sample = random.sample(self.pokemon_list, 1) # Randomly chooses 1 element from pokemon_list
+        return pokemon_sample
 
-def deck_building():
-    new_deck = choose_pokemon_random() # Calls the sample function
-    return new_deck
+    def deck_building(self):
+        new_deck = self.choose_pokemon_random() # Calls the sample function
+        return new_deck
+
+
+
+
+# Pokedex instance
+player_1 = Pokedex("player_deck")
 
 # deck created by calling the deck_building function
-print(deck_building())
+print(player_1.deck_building())
