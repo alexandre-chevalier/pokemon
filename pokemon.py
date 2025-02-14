5f7960e2971fb1ca7580d4732c6fd82d85dd36e1
alexandre <alexandre.chevalier@laplateforme.io>
2025-02-14 10:39:49 +0100

    i manage the pokemon list

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import json
 
 class Pokemon:
     def __init__(self, name, lifePoint, level, XP, giveXP, limitXP,  attack, defence, type1, type2, next_evolution):
@@ -17,8 +18,7 @@ class Pokemon:
         self.link_image = "images"
         self.statut = "normal"
         self.next_evolution = next_evolution
-
-    
+        self.pokemon_list = []
 
     def evolve(self, pokemon):
         if pokemon:
@@ -33,7 +33,6 @@ class Pokemon:
             self.type1 = pokemon.type1
             self.type2 = pokemon.type2
             self.next_evolution = pokemon.next_evolution
-
 
     def attacks(self):
         return (self.attack, self.defence)
@@ -54,6 +53,52 @@ class Pokemon:
         if self.lifePoint <= 0:
             self.KO = True
     
+
+#########################################################################################################################
+    def record_pokemon(self):
+        pokemon_dict_list = [pokemon.to_dict() for pokemon in self.pokemon_list]
+        with open('pokemon.json', 'w') as fichier:
+            json.dump(pokemon_dict_list, fichier,indent=4)
+
+    def get_pokemon_list(self):
+            try:
+                with open('pokemon.json', 'r') as fichier:
+                    pokemon_list = json.load(fichier)
+            except FileNotFoundError:
+                    pokemon_list = []
+            return pokemon_list
+
+    #  Build the list updated
+    def add_to_list(self, new_pokemon):
+
+            new_pokemon_dict = new_pokemon
+
+            # Vérifie si le Pokémon existe déjà dans la liste
+            if new_pokemon_dict not in self.pokemon_list:
+                self.pokemon_list.append(new_pokemon_dict)
+                self.record_pokemon()  # Enregistre la liste mise à jour
+            else:
+                print('this pokemon is already in your pokedex')
+
+    #name, lifePoint, level, XP, evolution, giveXP, limitXP,  attack, defence, type1, type2,
+        
+    def add_pokemon(self):
+            name = input("Nom du Pokémon : ")
+            pv = int(input("Points de vie : "))
+            type = input("Type : ")
+            attack = int(input("Attaque : "))
+            defense = int(input("Défense : "))
+            giveXP = int(input("Rapporte combien de points d'XP ? :"))
+
+            new_pokemon = {"name": name, "lifePoint": pv, "level" : 1, "XP" : 0,"evolution" : False, 
+                        "giveXP" : giveXP, "limitXP" :60, "attack": attack, "defense": defense, "type1": type, 
+                        "type2": None, "next_evolution" : self.next_evolution}
+            self.add_to_list(new_pokemon)
+            print(f"{name} a été ajouté !") 
+
+########################################################################################################################
+
+
     def to_dict(self):
         return {
             'name': self.name,
@@ -88,7 +133,7 @@ class Pokemon:
             self.evolve(self.next_evolution)
     
     def __str__(self):
-            return f"""
+            dictio =  f"""
                 name : {self.name}
                 lifepoint : {self.lifePoint}
                 level : {self.level}
@@ -101,9 +146,10 @@ class Pokemon:
                 type2 : {self.type2}
                 next_evolution : {self.next_evolution}
                 """
+            return dictio
 
 raichu = Pokemon("raichu", 250, 1, 0, 100, 120, 30,25, "electric", None, None)
-pikachu = Pokemon("Pikachu", 100, 1, 0, 10, 20,10, 8, "electric", None, raichu)
+pikachu = Pokemon("pikachu", 100, 1, 0, 10, 20,10, 8, "electric", None, raichu)
 tortank = Pokemon("tortank", 250, 1, 0, 100, 120, 30,25, "eau", None, None)
 carabaffe = Pokemon("carabaffe", 175, 1, 0, 60, 120, 30, 25, "eau", None, tortank)
 carapuce = Pokemon("carapuce", 120, 1, 0,60, 120, 30, 25, "eau",None, carabaffe)
@@ -121,5 +167,22 @@ grodoudou = Pokemon("grodoudou", 250, 1, 0, 60, 120, 30, 25, "normal", None,None
 rondoudou = Pokemon("rondoudou", 60, 1, 0, 60, 120, 50, 25, "normal", None,grodoudou)
 grotadmorv = Pokemon("grotadmorv", 175, 1,0, 100, 120, 30, 20,"poison", None, None)
 tadmorv = Pokemon("tadmorv", 120, 1,0, 100, 120, 30, 20,"poison", None, grotadmorv)
-ronflex = Pokemon("triopiqueur",250,1, 0, 60,120,30,25, "terre", None, None)
+ronflex = Pokemon("ronflex",250,1, 0, 60,120,30,25, "terre", None, None)
 hoho = Pokemon("hoho", 200, 1,0, 100, 120, 30, 20,"vol", "feu", None)
+
+pikachu.add_to_list(pikachu)
+pikachu.add_to_list(carapuce)
+pikachu.add_to_list(salameche)
+pikachu.add_to_list(bulbizarre)
+pikachu.add_to_list(lugia)
+pikachu.add_to_list(artikodin)
+pikachu.add_to_list(taupiqueur)
+pikachu.add_to_list(rondoudou)
+pikachu.add_to_list(tadmorv)
+pikachu.add_to_list(ronflex)
+pikachu.add_to_list(hoho)
+
+
+listing = pikachu.get_pokemon_list()
+
+print(listing)
